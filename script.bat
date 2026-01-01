151b486f972c2f0c896422eea3e01fb40d6670d4
Khushi Rajvanshi <khushirajvanshi203@gmail.com>
2026-01-01 12:01:00 +0530

    Commit on 2026-01-1

--- script.bat
+++ script.bat
@@ -1,21 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /L %%d in (1,1,20) do (
-  set /a skip=!random! %% 4
+set skipped=0
 
-  if !skip!==0 (
-    echo Skipping Day %%d
-  ) else (
-    set /a commits=!random! %% 3 + 1
+for %%m in (01 02 03) do (
+  for /L %%d in (1,1,28) do (
 
-    for /L %%i in (1,1,!commits!) do (
-      echo Commit %%d-%%i >> file.txt
-      git add .
-      set GIT_AUTHOR_DATE=2026-01-%%dT12:%%i:00
-      set GIT_COMMITTER_DATE=2026-01-%%dT12:%%i:00
-      git commit -m "Day %%d Commit %%i"
+    rem Allow only 6 skipped days total
+    if !skipped! LSS 6 (
+      set /a skip=!random! %% 10
+    ) else (
+      set skip=1
     )
+
+    if !skip!==0 (
+      echo Skipping 2026-%%m-%%d
+      set /a skipped+=1
+    ) else (
+      set /a commits=!random! %% 3 + 1
+
+      for /L %%i in (1,1,!commits!) do (
+        echo Commit %%m-%%d-%%i >> file.txt
+        git add .
+        set GIT_AUTHOR_DATE=2026-%%m-%%dT12:%%i:00
+        set GIT_COMMITTER_DATE=2026-%%m-%%dT12:%%i:00
+        git commit -m "Commit on 2026-%%m-%%d"
+      )
+    )
+
   )
 )
 
